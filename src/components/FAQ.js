f6589c232a1c4d38413dd70a62454007cd363b22
rusty-reebs <rusty_reebs@tutanota.com>
2022-06-07 18:16:51 -0600

    add FAQs and answers

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -3,7 +3,14 @@
 import React from "react";
 
 const FAQ = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <div className="flex">
+        <div>{/* FAQ card component */}</div>
+      </div>
+    </div>
+  );
 };
 
 export default FAQ;
