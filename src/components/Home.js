d5edd430849fa4bf2c8ab15c1437840d39431bb8
rusty-reebs <rusty_reebs@tutanota.com>
2022-06-16 17:07:06 -0600

    Add content to component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>MODERN AND UPSCALE LAUNDROMAT</p>
+      <h1>A Better Place to do Your Laundry</h1>
+      <button className="bg-gray-200 p-2">VIEW LOCATION</button>
+    </div>
+  );
 };
 
 export default Home;
