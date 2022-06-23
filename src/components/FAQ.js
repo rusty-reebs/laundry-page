0d279c3fe6327e0cd134ef0b2201466fb292d4d9
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-06-23 17:07:15 -0600

    styling faq component

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -5,20 +5,12 @@ import FAQcard from "./FAQcard";
 
 const FAQ = ({ content }) => {
   return (
-    <div>
-      <h1>Frequently Asked Questions</h1>
-      <div className="flex">
-        <div>
-          {content.map((faq) => {
-            return (
-              <FAQcard
-                key={faq.title}
-                title={faq.title}
-                content={faq.content}
-              />
-            );
-          })}
-        </div>
+    <div className="flex flex-col">
+      <h1 className="mx-auto">Frequently Asked Questions</h1>
+      <div className="flex flex-col lg:grid lg:grid-cols-2 lg:mx-40">
+        {content.map((faq) => (
+          <FAQcard key={faq.title} title={faq.title} content={faq.content} />
+        ))}
       </div>
     </div>
   );
