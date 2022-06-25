7bc7de8aa2dcf13933edff488493b586ca7dcff8
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-06-25 09:30:06 -0600

    install mantine and add accordion to FAQ

--- src/components/FAQcard.js
+++ src/components/FAQcard.js
@@ -1,26 +1,13 @@
 // FAQcard.js
 
 import React, { useState } from "react";
+import { Accordion } from "@mantine/core";
 
 const FAQcard = ({ title, content }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <div>
-      <div className={`py-5 ${isOpen ? "border-b border-slate-400" : null} `}>
-        <h2 className="font-bold" onClick={() => handleClick()}>
-          <span className="text-red-500 mr-2">+</span>
-          {title}
-        </h2>
-      </div>
-      <p className={` transition-all ease-out ${!isOpen ? "hidden" : "block"}`}>
-        {content}
-      </p>
-    </div>
+    <Accordion>
+      <Accordion.Item label={title}>{content}</Accordion.Item>
+    </Accordion>
   );
 };
 
