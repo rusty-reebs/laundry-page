2e25eef740d1ebde1f832a2f69d2e61303b2af34
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-06-29 17:24:32 -0600

    spacing for contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,16 +6,18 @@ import { ReactComponent as FacebookIcon } from "../img/facebook.svg";
 const Contact = ({ content }) => {
   console.log(content);
   return (
-    <div className="flex flex-col mt-10 lg:flex-row lg:mx-52">
+    <div className="flex flex-col mt-10 pb-20 lg:flex-row lg:mx-52">
       <div className="flex flex-col">
         <h1 className="text-3xl">Contact</h1>
         <FacebookIcon className="h-10 w-10 my-4" />
-        {content.map(({ title, detail }) => (
-          <div key={title} className="flex flex-col gap-y-5">
-            <h2 className="font-bold">{title.toUpperCase()}</h2>
-            <p>{detail}</p>
-          </div>
-        ))}
+        <div className="flex flex-col gap-y-20">
+          {content.map(({ title, detail }) => (
+            <div key={title} className="flex flex-col">
+              <h2 className="font-bold">{title.toUpperCase()}</h2>
+              <p>{detail}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
