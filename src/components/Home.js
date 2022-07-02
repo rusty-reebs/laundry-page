4d56cabcda734ba0c5352533f0bd07e55090adbe
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-07-02 13:10:57 -0600

    adjust photo positioning

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,21 @@
 // Home.js
 
 import React from "react";
-// import laundryPhoto from "../img/laundry-photo.jpg";
-const laundryPhoto = require("../img/laundry-photo3.jpg");
+import laundryPhoto from "../img/laundry-photo.jpg";
 
 const Home = () => {
   return (
-    <div
-      className="h-screen"
-      style={{
-        backgroundImage: `url(${laundryPhoto})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
-      <p>MODERN AND UPSCALE LAUNDROMAT</p>
-      <h1>A Better Place to do Your Laundry</h1>
-      <button className="bg-gray-200 p-2">VIEW LOCATION</button>
+    <div className="h-screen flex">
+      <div className="relative w-full mx-auto">
+        <img
+          src={laundryPhoto}
+          alt="cover"
+          className="h-1/2 absolute inset-0 mx-auto rounded-lg"
+        />
+        {/* <p>MODERN AND UPSCALE LAUNDROMAT</p> */}
+        {/* <h1>A Better Place to do Your Laundry</h1> */}
+        {/* <button className="bg-gray-200 p-2">VIEW LOCATION</button> */}
+      </div>
     </div>
   );
 };
