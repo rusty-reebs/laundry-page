ba535c371a9d987cd6b05dc50344a0e7e9f0e807
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-07-08 16:31:32 -0600

    center items on mobile

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const Services = ({ content }) => {
   return (
-    <div className="flex flex-col gap-y-5 max-w-full text-center lg:h-screen lg:flex-row lg:gap-x-20 lg:mx-32 lg:pt-52">
+    <div className="flex flex-col gap-y-5 mx-5 text-center lg:h-screen lg:flex-row lg:gap-x-20 lg:mx-32 lg:pt-52">
       {content.map((service) => (
         <div key={service.title} className="flex flex-col">
           <h3 className="text-lg font-bold pb-4">
