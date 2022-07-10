2f32075697b3e8e2066a9c495f5d397a4c5e8ec7
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-07-10 12:36:49 -0600

    add icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,7 +7,8 @@ const Services = ({ content }) => {
     <div className="flex flex-col gap-y-5 mx-5 text-center lg:h-screen lg:flex-row lg:gap-x-20 lg:mx-32 lg:pt-52">
       {content.map((service) => (
         <div key={service.title} className="flex flex-col">
-          <h3 className="text-lg font-bold pb-4">
+          <img src={service.icon} alt={service.title} className="mx-24" />
+          <h3 className="text-lg font-bold pb-4 pt-12">
             {service.title.toUpperCase()}
           </h3>
           <p>{service.description}</p>
