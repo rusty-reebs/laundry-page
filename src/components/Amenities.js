575cb97820a6091f52ebe4d09c13d3f8188d5091
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-07-10 12:38:22 -0600

    add and style icons

--- src/components/Amenities.js
+++ src/components/Amenities.js
@@ -4,16 +4,16 @@ import React from "react";
 
 const Amenities = ({ content }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col lg:h-screen">
       <h1 className="py-5 mx-auto text-3xl">
         We are on a mission to change the laundromat experience.
       </h1>
       <p className="mx-auto text-xl">Here's what we offer:</p>
-      <div className="flex flex-col max-w-full mt-10 lg:h-screen lg:grid lg:grid-cols-5 lg:gap-x-8 lg:mx-52">
+      <div className="flex flex-col max-w-full mt-10 lg:grid lg:grid-cols-5 lg:gap-x-8 lg:gap-y-20 lg:mx-52">
         {content.map((amenity) => (
-          <div key={amenity.title} className="flex flex-col w-32">
-            <img src={amenity.icon} alt="icon" height={30} />
-            <p>{amenity.title}</p>
+          <div key={amenity.title} className="flex flex-col gap-y-3">
+            <img src={amenity.icon} alt="icon" className="h-12" />
+            <p className="text-center">{amenity.title}</p>
           </div>
         ))}
       </div>
