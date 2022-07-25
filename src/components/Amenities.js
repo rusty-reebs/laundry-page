5847de8bb2aa48950bd3c06588a24c7d56665977
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-07-25 16:56:05 -0600

    little update

--- src/components/Amenities.js
+++ src/components/Amenities.js
@@ -6,7 +6,7 @@ const Amenities = ({ content }) => {
   return (
     <div className="flex flex-col pt-40">
       <h1 className="py-5 mx-auto text-3xl">
-        We are on a mission to change the laundromat experience.
+        We are on a mission to change the laundromat experience. YOU BETCHA!
       </h1>
       <p className="mx-auto text-xl">Here's what we offer:</p>
       <div className="flex flex-col max-w-full mt-20 lg:grid lg:grid-cols-5 lg:gap-x-8 lg:gap-y-20 lg:mx-52">
