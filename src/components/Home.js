a0669b84c5159b0c95f929c83e81c777e4c95e46
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-08-13 14:14:57 -0600

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
         <h1>A Better Place to do Your Laundry</h1>
         <h2>Come check us out!</h2>
         <h2>You'll be glad you did!</h2>
+        <h2>Serving the local area since 1999</h2>
         <button className="bg-gray-200 p-2">VIEW LOCATION</button>
       </div>
       <img src={laundryPhoto} alt="cover" className="h-2/3 rounded-lg" />
