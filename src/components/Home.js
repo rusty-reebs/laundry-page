ac3a4c56cc101affc6706f688d6b84cbcacc5859
Russell Salonen <83124600+rusty-reebs@users.noreply.github.com>
2022-08-16 05:24:49 -0600

    latest changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
         <h2>Come check us out!</h2>
         <h2>You'll be glad you did!</h2>
         <h2>Serving the local area since 1999</h2>
+        <h2>Ask us about our weekly specials!</h2>
         <button className="bg-gray-200 p-2">VIEW LOCATION</button>
       </div>
       <img src={laundryPhoto} alt="cover" className="h-2/3 rounded-lg" />
